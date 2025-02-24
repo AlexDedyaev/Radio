e817d63e3b09141806f5796a8a8f54aa57b4ac18
Alex Dedyaev <dedyaev1985@mail.ru>
2025-02-24 21:04:06 +0300

    Merging branches

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -2,7 +2,21 @@ package ru.netology;
 
 public class Radio {
     private int currentStation;
+    private int minStation = 0;
+    private int maxStation = 9;
     private int currentVolume;
+    private int minVolume = 0;
+    private int maxVolume = 100;
+
+    public Radio() {
+        this.currentStation = currentStation;
+        this.minStation = minStation;
+        this.maxStation = maxStation;
+        this.currentVolume = currentVolume;
+        this.minVolume = minVolume;
+        this.maxVolume = maxVolume;
+    }
+
 
     // Геттеры и сеттеры для текущей станции
     public int getCurrentStation() {
@@ -10,26 +24,27 @@ public class Radio {
     }
 
     public void setCurrentStation(int currentStation) {
-        if (currentStation >= 0 && currentStation <= 9) {
-            this.currentStation = currentStation;
+        if (currentStation < minStation || currentStation > maxStation) {
+            return;
         }
+        this.currentStation = currentStation;
     }
 
     // Метод для переключения на следующую станцию
-    public void next() {
-        if (currentStation == 9) {
-            currentStation = 0;
+    public void nextStation() {
+        if (maxStation <= currentStation) {
+            setCurrentStation(minStation);
         } else {
-            currentStation++;
+            setCurrentStation(currentStation + 1);
         }
     }
 
     // Метод для переключения на предыдущую станцию
-    public void prev() {
-        if (currentStation == 0) {
-            currentStation = 9;
+    public void previousStation() {
+        if (currentStation <= minStation) {
+            setCurrentStation(maxStation);
         } else {
-            currentStation--;
+            setCurrentStation(currentStation - 1);
         }
     }
 
@@ -39,17 +54,19 @@ public class Radio {
     }
 
     public void setCurrentVolume(int currentVolume) {
-        if (currentVolume >= 0 && currentVolume <= 100) {
-            this.currentVolume = currentVolume;
+        if (currentVolume < minVolume || currentVolume > maxVolume) {
+            return;
         }
+        this.currentVolume = currentVolume;
     }
 
     // Метод для увеличения громкости на один
     public void increaseVolume() {
-        if (currentVolume < 100) {
+        if (currentVolume < maxVolume) {
             currentVolume++;
         }
     }
+
 
     // Метод для уменьшения громкости на один
     public void decreaseVolume() {
