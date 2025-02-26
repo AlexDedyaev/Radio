5aa51395768e508982abd47dd9779c068d850799
Alex Dedyaev <154517065+AlexDedyaev@users.noreply.github.com>
2025-02-26 21:01:17 +0300

    Update Radio.java

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -4,14 +4,18 @@ public class Radio {
     private int currentStation;
     private int minStation = 0;
     private int maxStation = 9;
+    private int currentVolume;
+    private int minVolume = 0;
+    private int maxVolume = 100;
 
     public Radio() {
         this.currentStation = currentStation;
         this.minStation = minStation;
         this.maxStation = maxStation;
-
+        this.currentVolume = currentVolume;
+        this.minVolume = minVolume;
+        this.maxVolume = maxVolume;
     }
-
     // Геттеры и сеттеры для текущей станции
     public int getCurrentStation() {
         return currentStation;
@@ -40,5 +44,43 @@ public class Radio {
         } else {
             setCurrentStation(currentStation - 1);
         }
+    }
+    // Геттеры и сеттеры для текущей громкости
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume >= 0 && currentVolume <= 100) {
+            this.currentVolume = currentVolume;
+        }
+    }
+
+    // Метод для увеличения громкости на один
+    public void increaseVolume() {
+        if (currentVolume < 100) {
+            currentVolume++;
+        }
+    }
+
+    // Метод для уменьшения громкости на один
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume--;
+        }
+    }
+    public int getMaxRadioStation() {
+        return maxStation;
+    }
+    public void setMaxRadioStation(int maxRadioStation) {
+        this.maxStation = maxRadioStation;
+    }
+
+    public int getMinRadioStation() {
+        return minStation;
+    }
+
+    public void setMinRadioStation(int minRadioStation) {
+        this.minStation = minRadioStation;
     }
 }
